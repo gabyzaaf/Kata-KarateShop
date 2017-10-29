b02b6a7f0448254913719aa5604229f71e54b5cc
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-29 12:41:01 +0100

    add Sort method

---
+++ KarateKataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KarateKataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KarateKataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("866d0b9c-9150-4b0e-9164-a4862c3b0d96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KarateKataTest/RankingLibrary.cs
@@ -0,0 +1,51 @@
+﻿namespace KarateKataTest
+{
+    internal class RankingLibrary
+    {
+        private int[] arrays = null;
+
+        public RankingLibrary(int[] arrays)
+        {
+            this.arrays = arrays;
+        }
+
+       
+        
+
+        public void Sort()
+        {
+            int[] arraysEvalued = arrays;
+            if (arrays.Length == 1)
+            {
+                arraysEvalued = arrays;
+            }
+            int numberInferrior = 0;
+            int numberSupperior = 0;
+            for (int i = 0; i < arrays.Length - 1; i++)
+            {
+                if (arrays[i] > arrays[i + 1])
+                {
+                    numberInferrior = arrays[i + 1];
+                    arrays[i + 1] = arrays[i];
+                    arrays[i] = numberInferrior;
+                    if (i > 0)
+                    {
+                        for (int j = i; j > 0; j--)
+                        {
+                            if (j != 0)
+                            {
+                                if (numberInferrior < arrays[j - 1])
+                                {
+                                    numberSupperior = arrays[j - 1];
+                                    arrays[j - 1] = numberInferrior;
+                                    arrays[j] = numberSupperior;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return arraysEvalued;
+        }
+    }
+}
